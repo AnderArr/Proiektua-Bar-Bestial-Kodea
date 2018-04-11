5329b5e3aadb52865ebfaaccd8164d39e4c29aa4
urtzipu <37211750+urtzipu@users.noreply.github.com>
2018-04-11 12:32:45 +0200

    Update Tableroa.java

--- src/packProiektua/Tableroa.java
+++ src/packProiektua/Tableroa.java
@@ -4,53 +4,42 @@ package packProiektua;
 
 
 public class Tableroa {
-	private KartaZerrenda J1;
-	private KartaZerrenda CPU;
+	private JokalariErreala J1;
+	private CPUJokalaria CPU;
 	private KartaZerrenda kola;
 	private KartaZerrenda sartuakJ1;
 	private KartaZerrenda sartuakCPU;
-	private KartaZerrenda eskuanJ1;
-	private KartaZerrenda eskuanCPU;
 	private static Tableroa nireTableroa;
 	
 	private Tableroa(){
-		J1=new KartaZerrenda();
-		CPU=new KartaZerrenda();
+		J1=new JokalariErreala();
+		CPU=new CPUJokalaria();
 		kola=new KartaZerrenda();
 		sartuakJ1=new KartaZerrenda();
 		sartuakCPU=new KartaZerrenda();
-		eskuanJ1=new KartaZerrenda();
-		eskuanCPU=new KartaZerrenda();	
+		
 	}
-	
 	public KartaZerrenda getJ1(){
-		return this.J1;
+		return this.J1.getBaraja();
 	}
-	
 	public KartaZerrenda getEskuanJ1(){
-		return this.eskuanJ1;
+		return this.J1.getEskuan();
 	}
-	
 	public KartaZerrenda getCPU(){
-		return this.CPU;
+		return this.CPU.getBaraja();
 	}
-	
 	public KartaZerrenda getEskuanCPU(){
-		return this.eskuanCPU;
+		return this.CPU.getEskuan();
 	}
-	
 	public KartaZerrenda getSartuakJ1(){
 		return this.sartuakJ1;
 	}
-	
 	public KartaZerrenda getSartuakCPU(){
 		return this.sartuakCPU;
 	}
-	
 	public KartaZerrenda getKola(){
 		return this.kola;
 	}
-	
 	public static Tableroa getNireTableroa(){
 		if(nireTableroa==null){
 			nireTableroa=new Tableroa();
@@ -59,23 +48,9 @@ public class Tableroa {
 	}
 	
 	
-	private void sortuEskua(KartaZerrenda e1,KartaZerrenda b1){
-		
-		Karta k1=b1.kartaHartu();
-		Karta k2=b1.kartaHartu();
-		Karta k3=b1.kartaHartu();
-		Karta k4=b1.kartaHartu();
-		e1.kartaGehitu(k1);
-		e1.kartaGehitu(k2);
-		e1.kartaGehitu(k3);
-		e1.kartaGehitu(k4);
+	public void jokoaHasieratu(){
+		J1.jokalariaHasieratu("Urdina");
+		CPU.jokalariaHasieratu("Berdea");
 	}
 	
-	public void jokoaHasieratu(){
-		Tableroa t=Tableroa.getNireTableroa();
-		t.sortuBaraja("Urdina", J1);
-		t.sortuBaraja("Gorria", CPU);
-		t.sortuEskua(eskuanJ1, J1);
-		t.sortuEskua(eskuanCPU, CPU);
-	}
 }
