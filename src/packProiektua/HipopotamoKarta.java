7a18887befc3dd811e7567fcb8ac5c65a17a8ff7
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-04-12 13:03:36 +0200

    Hipopotamoaren animalada
    Hipopotamoaren animalada

--- src/packProiektua/HipopotamoKarta.java
+++ src/packProiektua/HipopotamoKarta.java
@@ -7,7 +7,21 @@ public class HipopotamoKarta extends Karta{
 	}
 	
 	public void gaitasunaBurutu(){
-		
+		Tableroa t= Tableroa.getNireTableroa();
+		KartaZerrenda kZ = t.getKola();
+		if (kZ.luzera()>1){
+			boolean irten=false;
+			Karta aux = kZ.getKarta(kZ.luzera()-1);
+			while((aux.getBalioa()<this.balioa)&&(!aux.getIzena().equals("Zebra")&&(!irten))){
+				kZ.elkartrukatu(aux, this, kZ.posizioaLortu(aux), kZ.posizioaLortu(this));
+				int ind =kZ.posizioaLortu(this);
+				if (ind<1){
+					aux=kZ.getKarta(ind);
+				} else {
+					irten=true;
+				}
+			}
+		}
 	}
 	
 
