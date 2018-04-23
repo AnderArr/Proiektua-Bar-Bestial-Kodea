9014052bdd2d4b11c00e04b46a24e4ad6af93c48
urtzipu <37211750+urtzipu@users.noreply.github.com>
2018-04-23 15:39:54 +0200

    Update TximinoKarta.java

--- src/packProiektua/TximinoKarta.java
+++ src/packProiektua/TximinoKarta.java
@@ -1,38 +1,16 @@
 package packProiektua;
 
-import java.util.Iterator;
 
 public class TximinoKarta extends Karta{
 	
 	
 	public TximinoKarta(String pKolorea){
 		super("Tximinoa",4,false,pKolorea,"<html>Jokoan sartzean,<br> beste tximino bat<br> aurrean badauka,<br> hipopotamoak eta <br>krokodiloak kalera  <br>botatzen ditu eta  <br>tximinoak lehendabizikoak <br>jartzen dira.<br></html>",3);
+	this.ia= new TximinoAnimalada();
 	}
 	
-	public void gaitasunaBurutu(){
-		Tableroa t= Tableroa.getNireTableroa();
-		KartaZerrenda kZ = t.getKola();
-		Iterator<Karta> itr = kZ.getIteradorea();
-		Karta k1=itr.next();
-		boolean irten=false;
-		while((k1!=this) && (!irten)){
-			if (k1.getIzena().equals(this.izena)){
-				irten=true;
-			}
-			k1=itr.next();
-		}
-		if (irten){
-			Iterator<Karta> itr2 = kZ.getIteradorea();
-			Karta aux = itr2.next();
-			while(aux!=this){
-				if (aux.getIzena().equals("Hipotamoa")||aux.getIzena().equals("Kokodriloa")){
-					kZ.kartaEzabatu(aux);
-				}
-				aux=itr2.next();
-			}
-			kZ.hasieranJarri(k1);
-			kZ.hasieranJarri(this);
-		}
+	public void gaitasunaBurutu(int a){
+		this.ia.gaitasunaBurutu(a);
 	}
 	
 
