21f41398f41df17afe7a7d97cf2a6e36885ade84
urtzipu <37211750+urtzipu@users.noreply.github.com>
2018-04-23 15:38:31 +0200

    Update SugeKarta.java

--- src/packProiektua/SugeKarta.java
+++ src/packProiektua/SugeKarta.java
@@ -5,24 +5,12 @@ public class SugeKarta extends Karta{
 	
 	public SugeKarta(String pKolorea){
 		super("Sugea",9,true,pKolorea,"<html>Jokoan sartzean,<br> ilara ordenatzen du,<br> balio handienak atetik <br>hurbil uzten eta <br>txikienak kaletik <br>hurbil.<br><br><br></html>",2);
+		this.ia=new SugeAnimalada();
+		}
+	
+	public void gaitasunaBurutu(int a){
+		this.ia.gaitasunaBurutu(a);
 	}
 	
-	public void gaitasunaBurutu(){
-		Tableroa t= Tableroa.getNireTableroa();
-		KartaZerrenda kZ = t.getKola();
-		int luzera=kZ.luzera();
-		Karta aux1=null;
-		Karta aux2=null;
-		for (int ind=0; ind<luzera;ind++){
-			aux1=kZ.getKarta(ind);
-			for (int ind2=ind; ind2<luzera; ind2++){
-				aux2=kZ.getKarta(ind2);
-				if (aux2.getBalioa()>aux1.getBalioa()){
-					aux1=aux2;
-				}
-			}
-			kZ.elkartrukatu(kZ.getKarta(0), aux1, ind, kZ.posizioaLortu(aux1));
-		}
-		
-	}
+
 }
