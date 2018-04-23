4c902324513b97032c83eb8bde9ee2c06603cac0
urtzipu <37211750+urtzipu@users.noreply.github.com>
2018-04-23 08:35:51 +0200

    Update JokalariErreala.java

--- src/packProiektua/JokalariErreala.java
+++ src/packProiektua/JokalariErreala.java
@@ -6,10 +6,13 @@ public class JokalariErreala extends Jokalaria {
 		super();
 	}
 	
-	public void kartaMahairaBota(int pPos){
+	public void kartaMahairaBota(int pPos, int aux){
 		Tableroa t= Tableroa.getNireTableroa();
 		Karta k= this.eskuan.getKarta(pPos);
 		this.eskuan.kartaEzabatu(k);
 		t.getKola().kartaGehitu(k);
+		if(t.getKola().luzera()>1){
+			k.gaitasunaBurutu(aux);
+		}
 	}
 }
