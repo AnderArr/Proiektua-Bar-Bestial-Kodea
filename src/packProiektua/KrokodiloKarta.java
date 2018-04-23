9df3ff99df11ed3d817bbc229f2a4f4e99923f92
urtzipu <37211750+urtzipu@users.noreply.github.com>
2018-04-23 15:31:50 +0200

    Update KrokodiloKarta.java

--- src/packProiektua/KrokodiloKarta.java
+++ src/packProiektua/KrokodiloKarta.java
@@ -5,24 +5,12 @@ public class KrokodiloKarta extends Karta{
 	
 	public KrokodiloKarta(String pKolorea){
 		super("Krokodiloa",10,true,pKolorea, "<html>Jokoan sartzean,<br> bera baino balio<br> txikiagoak dituen kartak<br> jan, eta kalera<br> botatzen ditu.<br> Beti ere Zebra<br> bat aurretik ez badauka.<br> Errepikakorra da.</html>",3);
+		this.ia= new KrokodiloAnimalada();
 	}
 	
-	public void gaitasunaBurutu(){
-		Tableroa t= Tableroa.getNireTableroa();
-		KartaZerrenda kZ = t.getKola();
-		if (kZ.luzera()>1){
-			boolean irten=false;
-			Karta aux= kZ.getKarta(kZ.luzera()-1);
-			while ((aux.getBalioa()<this.balioa)&&(!irten)){
-				kZ.kartaEzabatu(aux);
-				int ind=kZ.posizioaLortu(this);
-				if (ind>1){
-					aux=kZ.getKarta(ind-1);
-				} else {
-					irten=true;
-				}
-				
-			}
-		}
+	public void gaitasunaBurutu(int a){
+		this.ia.gaitasunaBurutu(a);
 	}
+	
+
 }
