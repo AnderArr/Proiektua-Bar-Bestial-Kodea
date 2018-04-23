dabb8f0448af7d13a4e68456e1a31c70506d7403
urtzipu <37211750+urtzipu@users.noreply.github.com>
2018-04-23 08:30:18 +0200

    Update HipopotamoKarta.java

--- src/packProiektua/HipopotamoKarta.java
+++ src/packProiektua/HipopotamoKarta.java
@@ -4,25 +4,11 @@ public class HipopotamoKarta extends Karta{
 	
 	public HipopotamoKarta(String pKolorea){
 		super("Hipopotamoa",11,true,pKolorea,"<html>Jokoan sartzean,<br> bere balioa baino<br> txikiagoak diren<br> karten aurretik<br> kokatzen da,<br> beti ere Zebra<br> bat aurretik ez badago.<br> Errepikakorra da.</html>",2);
+		this.ia= new HipopotamoAnimalada();
 	}
 	
-	public void gaitasunaBurutu(){
-		Tableroa t= Tableroa.getNireTableroa();
-		KartaZerrenda kZ = t.getKola();
-		if (kZ.luzera()>1){
-			boolean irten=false;
-			Karta aux = kZ.getKarta(kZ.luzera()-1);
-			while((aux.getBalioa()<this.balioa)&&(!aux.getIzena().equals("Zebra")&&(!irten))){
-				kZ.elkartrukatu(aux, this, kZ.posizioaLortu(aux), kZ.posizioaLortu(this));
-				int ind =kZ.posizioaLortu(this);
-				if (ind>1){
-					aux=kZ.getKarta(ind-1);
-				} else {
-					irten=true;
-				}
-			}
-		}
+	public void gaitasunaBurutu(int a){
+		this.ia.gaitasunaBurutu(a);
 	}
-	
 
 }
