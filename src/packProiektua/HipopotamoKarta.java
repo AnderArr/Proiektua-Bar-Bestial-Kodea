46a5027b31716544999c1c930ef44f56d457a78d
megander56 <37210967+megander56@users.noreply.github.com>
2018-04-26 10:38:59 +0200

    Update HipopotamoKarta.java

--- src/packProiektua/HipopotamoKarta.java
+++ src/packProiektua/HipopotamoKarta.java
@@ -5,10 +5,12 @@ public class HipopotamoKarta extends Karta{
 	public HipopotamoKarta(String pKolorea){
 		super("Hipopotamoa",11,true,pKolorea,"<html>Jokoan sartzean,<br> bere balioa baino<br> txikiagoak diren<br> karten aurretik<br> kokatzen da,<br> beti ere Zebra<br> bat aurretik ez badago.<br> Errepikakorra da.</html>",2);
 		this.ia= new HipopotamoAnimalada();
-	}
+		}
+		
+		public void gaitasunaBurutu(int a,int b,Karta k){
+			this.ia.gaitasunaBurutu(a,b,this);
+		}
+
 	
-	public void gaitasunaBurutu(int a){
-		this.ia.gaitasunaBurutu(a);
-	}
 
 }
