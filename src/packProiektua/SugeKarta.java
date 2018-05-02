f22ebffec09d356d2b4f114eaa0cc16af1cb74a1
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-02 13:44:45 +0200

    Update SugeKarta.java

--- src/packProiektua/SugeKarta.java
+++ src/packProiektua/SugeKarta.java
@@ -2,15 +2,12 @@ package packProiektua;
 
 public class SugeKarta extends Karta{
 	
-	
 	public SugeKarta(String pKolorea){
-		super("Sugea",9,true,pKolorea,"<html>Jokoan sartzean,<br> ilara ordenatzen du,<br> balio handienak atetik <br>hurbil uzten eta <br>txikienak kaletik <br>hurbil.<br><br><br></html>",2);
+		super("Sugea",9,false,pKolorea,"<html>Jokoan sartzean,<br> ilara ordenatzen du,<br> balio handienak atetik <br>hurbil uzten eta <br>txikienak kaletik <br>hurbil.<br><br><br></html>",2);
 		this.ia=new SugeAnimalada();
-		}
-	
-	public void gaitasunaBurutu(int a){
-		this.ia.gaitasunaBurutu(a);
 	}
-	
 
+	public void gaitasunaBurutu(int a, int b,Karta k){
+		this.ia.gaitasunaBurutu(a,b, this);
+	}
 }
