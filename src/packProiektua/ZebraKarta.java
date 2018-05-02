d0910202d02463f0ce370168d77a26ac78547b26
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-02 13:50:00 +0200

    Update ZebraKarta.java

--- src/packProiektua/ZebraKarta.java
+++ src/packProiektua/ZebraKarta.java
@@ -2,14 +2,12 @@ package packProiektua;
 
 public class ZebraKarta extends Karta{
 	
-	
 	public ZebraKarta(String pKolorea){
 		super("Zebra",7,true,pKolorea,"<html>Jokoan sartzean,<br> Hipopotamoen eta<br> Krokodiloen mugimendua<br> gelditzen du.<br> Errepikakorra da.<br><br><br><br></html>",4);
 		this.ia= new ZebraAnimalada();
-		}
-	
-	public void gaitasunaBurutu(int a){
-		this.ia.gaitasunaBurutu(a);
 	}
 
+	public void gaitasunaBurutu(int a,int b, Karta k){
+		this.ia.gaitasunaBurutu(a,b, this);
+	}
 }
