ba99e086705514f1eec3b5b48bb42be22f69f2f2
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-02 13:45:25 +0200

    Update Tableroa.java

--- src/packProiektua/Tableroa.java
+++ src/packProiektua/Tableroa.java
@@ -17,33 +17,40 @@ public class Tableroa extends Observable{
 		kola=new KartaZerrenda();
 		sartuakJ1=new KartaZerrenda();
 		sartuakCPU=new KartaZerrenda();
-		
 	}
 	
 	public JokalariErreala get(){
 		return this.J1;
 	}
+	
 	public KartaZerrenda getJ1(){
 		return this.J1.getBaraja();
 	}
+	
 	public KartaZerrenda getEskuanJ1(){
 		return this.J1.getEskuan();
 	}
+	
 	public KartaZerrenda getCPU(){
 		return this.CPU.getBaraja();
 	}
+	
 	public KartaZerrenda getEskuanCPU(){
 		return this.CPU.getEskuan();
 	}
+	
 	public KartaZerrenda getSartuakJ1(){
 		return this.sartuakJ1;
 	}
+	
 	public KartaZerrenda getSartuakCPU(){
 		return this.sartuakCPU;
 	}
+	
 	public KartaZerrenda getKola(){
 		return this.kola;
 	}
+	
 	public static Tableroa getNireTableroa(){
 		if(nireTableroa==null){
 			nireTableroa=new Tableroa();
@@ -59,19 +66,17 @@ public class Tableroa extends Observable{
 		}
 	}
 	
-	
-	
-	public void txandaJokatu(int pos,int aux){
-	    J1.kartaMahairaBota(pos,aux);
+	public void txandaJokatu(int pos,int aux,int aux2){
+	    J1.kartaMahairaBota(pos,aux,aux2);
 	    J1.kartaBarajatikHartu();
-	    //animaladaRekurrenteak();
+	    animaladaRekurrenteak();
 	    if(this.kola.luzera()==5){
 	    	this.kolaEguneratu();
 	    }
 	    
-		//CPU.kartaMahairaBota();
-		//CPU.kartaBarajatikHartu();
-		//animaladaRekurrenteak();
+		CPU.kartaMahairaBota();
+		CPU.kartaBarajatikHartu();
+		animaladaRekurrenteak();
 		if(this.kola.luzera()==5){
 	    	this.kolaEguneratu();
 	    }
@@ -85,7 +90,7 @@ public class Tableroa extends Observable{
 		}
 		setChanged();
 		notifyObservers(bz);
-}
+	}
 
 	public boolean kamalehoiaDa(int pPos){
 		return this.getEskuanJ1().kamalehoiaDa(pPos);
@@ -98,8 +103,6 @@ public class Tableroa extends Observable{
 	public boolean kanguroaDaKolan(int pPos){
 		return this.kola.kanguroaDa(pPos);
 	}
-	
-	
 	
 	public boolean loroaDa(int pPos){
 		return this.getEskuanJ1().loroaDa(pPos);
@@ -117,76 +120,78 @@ public class Tableroa extends Observable{
 		return this.getEskuanJ1().kanguroaDa(pPos);
 	}
 
-public void animaladaRekurrenteak(){
-	for(int i=0;i<this.kola.luzera();i++){
-		Karta k= this.kola.getKarta(i);
-		if (k.getErrepikatu() && this.kola.luzera()>1){
-			k.gaitasunaBurutu(0);
+	public void animaladaRekurrenteak(){
+		int ind=0;
+		KartaZerrenda kZ=this.kola;
+		Karta aux;
+		while(ind<kZ.luzera()){
+			Karta k=kZ.getKarta(ind);
+			aux=null;
+			if((k.getErrepikatu())&&(kZ.luzera()>1)){
+				if ((kZ.luzera()-1)!=kZ.posizioaLortu(k)){
+					aux=kZ.getKarta(ind+1);
+				}
+				k.gaitasunaBurutu(0, 0, k);
+			}
+			if(aux!=null){
+				ind=kZ.posizioaLortu(aux);	
+			}else{
+				ind++;
+			}
 		}
 	}
-}
 
-public boolean bukatuta(){
-	return ((this.J1.getEskuan().luzera()==0)&&(this.CPU.getEskuan().luzera()==0));
-}
+	public boolean bukatuta(){
+		return ((this.J1.getEskuan().luzera()==0)&&(this.CPU.getEskuan().luzera()==0));
+	}
 
-public void kolaEguneratu(){
-	this.kola.kartaKendu(4);
-	Karta k1= this.kola.kartaHartu();
-	Karta k2= this.kola.kartaHartu();
-	this.sartuBereLekuan(k1);
-	this.sartuBereLekuan(k2);
-	
-}
+	public void kolaEguneratu(){
+		this.kola.kartaKendu(4);
+		Karta k1= this.kola.kartaHartu();
+		Karta k2= this.kola.kartaHartu();
+		this.sartuBereLekuan(k1);
+		this.sartuBereLekuan(k2);
+	}
 
-public String irabazleaLortu(){
-	int punt1=0;
-	int punt2=0;
-	for(int i=0;i<this.sartuakJ1.luzera();i++){
-		punt1=punt1+this.sartuakJ1.getKarta(i).getBalioa();
-	}
-	for(int j=0;j<this.sartuakCPU.luzera();j++){
-		punt2=punt2+this.sartuakCPU.getKarta(j).getBalioa();
-	}
-	if (punt1>punt2){
-		return "J1 irabazi du";
-	}
-	else{
-		if(punt1<punt2){
-			return "CPU irabazi du";
+	public String irabazleaLortu(){
+		int punt1=0;
+		int punt2=0;
+		for(int i=0;i<this.sartuakJ1.luzera();i++){
+			punt1=punt1+this.sartuakJ1.getKarta(i).getBalioa();
+		}
+		for(int j=0;j<this.sartuakCPU.luzera();j++){
+			punt2=punt2+this.sartuakCPU.getKarta(j).getBalioa();
+		}
+		if (punt1>punt2){
+			return "J1 irabazi du";
 		}
 		else{
-			return "Berdinketa egon da";
+			if(punt1<punt2){
+				return "CPU irabazi du";
+			}
+			else{
+				return "Berdinketa egon da";
+			}
+		}	
+	}
+
+	public void sartuBereLekuan(Karta k1){
+		if(k1.getKolorea().equals("Urdina")){
+			this.sartuakJ1.kartaGehitu(k1);
+		}else{
+			this.sartuakCPU.kartaGehitu(k1);
 		}
 	}
-	
-}
 
-
-public void sartuBereLekuan(Karta k1){
-	if(k1.getKolorea().equals("Urdina")){
-		this.sartuakJ1.kartaGehitu(k1);
-	}else{
-		this.sartuakCPU.kartaGehitu(k1);
+	public boolean kamalehoiaDa(String a){
+		KamalehoiKarta k=new KamalehoiKarta("berdea");
+		if(a.equals(k.getKartaIrudia())){
+			return true;
+		}else{
+			return false;
+		}
 	}
-}
 
-public boolean kamalehoiaDa(String a){
-	KamalehoiKarta k=new KamalehoiKarta("berdea");
-	if(a.equals(k.getKartaIrudia())){
-		return true;
-	}else{
-		return false;
-	}
-}
-
-
-
-
-
-
-	
-	
 	public void jokoaHasieratu(){
 		J1.jokalariaHasieratu("Urdina");
 		CPU.jokalariaHasieratu("Berdea");
@@ -199,9 +204,6 @@ public boolean kamalehoiaDa(String a){
 			bz.add(this.J1.getEskuan().getKarta(ind).getKartaIrudia());
 		}
 		setChanged();
-		notifyObservers(bz);
-		
-		
+		notifyObservers(bz);		
 	}
-	
 }
