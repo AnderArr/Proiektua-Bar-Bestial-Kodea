b313224f28fa6d86b7028732db312df1c6e6a8e4
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-02 13:42:11 +0200

    Update LehoiKarta.java

--- src/packProiektua/LehoiKarta.java
+++ src/packProiektua/LehoiKarta.java
@@ -1,19 +1,13 @@
 package packProiektua;
 
-import java.util.Iterator;
-
 public class LehoiKarta extends Karta{
 	
-	
 	public LehoiKarta(String pKolorea){
-		super("Lehoia",12,true,pKolorea,"<html>Jokoan sartzean, ilaran<br> beste lehoirik ez badago,<br> lehendabizikoa kokatzen <br>da eta tximinoak kalera<br> botatzen ditu baina<br> beste lehoi bat<br> baldin badago karta<br> bera kalera doa.</html>",2);
+		super("Lehoia",12,false,pKolorea,"<html>Jokoan sartzean, ilaran<br> beste lehoirik ez badago,<br> lehendabizikoa kokatzen <br>da eta tximinoak kalera<br> botatzen ditu baina<br> beste lehoi bat<br> baldin badago karta<br> bera kalera doa.</html>",2);
 		this.ia= new LehoiAnimalada();
 	}
 	
 	public void gaitasunaBurutu(int a, int b, Karta k){
 		this.ia.gaitasunaBurutu(a,b, this);
 	}
-	
-
 }
-
