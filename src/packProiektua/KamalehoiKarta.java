69092164fbc166b95be98ae0217a0d97f0adc87f
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-02 13:37:06 +0200

    Update KamalehoiKarta.java

--- src/packProiektua/KamalehoiKarta.java
+++ src/packProiektua/KamalehoiKarta.java
@@ -2,7 +2,6 @@ package packProiektua;
 
 public class KamalehoiKarta extends Karta{
 	
-	
 	public KamalehoiKarta(String pKolorea){
 		super("Kamalehoia",5,false,pKolorea,"<html>Jokoan sartzean,<br> ilarako beste animalia<br> baten gaitasuna<br> bereganatzen du,<br> ondoren kamalehoi bat<br> izatera bueltatzen da.<br><br><br></html>",3);
 		this.ia= new KamalehoiAnimalada();
@@ -11,5 +10,4 @@ public class KamalehoiKarta extends Karta{
 	public void gaitasunaBurutu(int pos,int b, Karta k){
 		this.ia.gaitasunaBurutu(pos,b,this);
 	}
-
 }
