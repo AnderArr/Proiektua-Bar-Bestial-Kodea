e4fda8b8ef25363f6c5c4419ed4aabb7da319fc9
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-02 13:42:55 +0200

    Update LoroKarta.java

--- src/packProiektua/LoroKarta.java
+++ src/packProiektua/LoroKarta.java
@@ -2,14 +2,12 @@ package packProiektua;
 
 public class LoroKarta extends Karta{
 	
-	
 	public LoroKarta(String pKolorea){
 		super("Loroa",2,false,pKolorea,"<html>Jokoan sartzean,<br> jokalariak nahi duen<br> animalia kanporatzen<br> du.<br><br><br><br><br></html>",4);
 		this.ia= new LoroAnimalada();
 	}
 	
-	public void gaitasunaBurutu(int pos){
-		this.ia.gaitasunaBurutu(pos);
+	public void gaitasunaBurutu(int pos,int b, Karta k){
+		this.ia.gaitasunaBurutu(pos,b, this);
 	}
-
 }
