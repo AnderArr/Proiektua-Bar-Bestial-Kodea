06a4325f795251f56d6b531d418f11c8c15dd363
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-20 21:17:15 +0200

    Update MofetaKarta.java

--- src/packProiektua/MofetaKarta.java
+++ src/packProiektua/MofetaKarta.java
@@ -1,9 +1,11 @@
 package packProiektua;
 
+import java.util.Iterator;
+
 public class MofetaKarta extends Karta{
 	
 	public MofetaKarta(String pKolorea){
-		super("Mofeta",1,false,pKolorea,"<html>Jokoan sartzean,<br> ilarako balio altueneko<br> bi espezieak<br> kanporatzen ditu.<br><br><br><br><br></html>",4);
+		super("Mofeta",1,false,pKolorea,"<html>Jokoan sartzean,<br> ilarako balio altueneko<br> bi espezieak<br> kanporatzen ditu.<br><br><br><br><br></html>",4,"C:/Kartak/Mofeta"+pKolorea+".png");
 		this.ia=new MofetaAnimalada();
 	}
 
