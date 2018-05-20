31794d35c5592d9a8ad6a438fdf8bdd0af77b601
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-20 21:32:46 +0200

    Update TximinoKarta.java

--- src/packProiektua/TximinoKarta.java
+++ src/packProiektua/TximinoKarta.java
@@ -3,7 +3,7 @@ package packProiektua;
 public class TximinoKarta extends Karta{
 	
 	public TximinoKarta(String pKolorea){
-		super("Tximinoa",4,false,pKolorea,"<html>Jokoan sartzean,<br> beste tximino bat<br> aurrean badauka,<br> hipopotamoak eta <br>krokodiloak kalera  <br>botatzen ditu eta  <br>tximinoak lehendabizikoak <br>jartzen dira.<br></html>",3);
+		super("Tximinoa",4,false,pKolorea,"<html>Jokoan sartzean,<br> beste tximino bat<br> aurrean badauka,<br> hipopotamoak eta <br>krokodiloak kalera  <br>botatzen ditu eta  <br>tximinoak lehendabizikoak <br>jartzen dira.<br></html>",3,"C:/Kartak/Tximino"+pKolorea+".png");
 		this.ia= new TximinoAnimalada();
 	}
 	
