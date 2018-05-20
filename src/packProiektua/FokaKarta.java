d7173774eb0b8507174303e5030fed55f76af8ac
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-20 21:03:47 +0200

    Update FokaKarta.java

--- src/packProiektua/FokaKarta.java
+++ src/packProiektua/FokaKarta.java
@@ -1,13 +1,18 @@
 package packProiektua;
 
+import java.util.Iterator;
+
 public class FokaKarta extends Karta{
 	
+	
 	public FokaKarta(String pKolorea){
-		super("Foka",6,false,pKolorea,"<html>Jokoan sartzean,<br>ilararen ordena aldatzen<br>  du buelta emanez.<br> <br><br><br><br><br></html>",2);
+		super("Foka",6,false,pKolorea,"<html>Jokoan sartzean,<br>ilararen ordena aldatzen<br>  du buelta emanez.<br> <br><br><br><br><br></html>",2,"C:/Kartak/Foka"+pKolorea+".png");
 		this.ia= new FokaAnimalada();
 	}
 	
 	public void gaitasunaBurutu(int a,int b, Karta k){
 				ia.gaitasunaBurutu(a,b,this);
 	}
+
+	
 }
