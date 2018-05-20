9a9b8d8e96c21897447e293a9ad5febad9ea38d9
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-20 21:27:30 +0200

    Update Tableroa.java

--- src/packProiektua/Tableroa.java
+++ src/packProiektua/Tableroa.java
@@ -1,7 +1,12 @@
 package packProiektua;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Observable;
+import java.sql.*;
+import java.util.Date;
 
 public class Tableroa extends Observable{
 	private JokalariErreala J1;
@@ -10,48 +15,58 @@ public class Tableroa extends Observable{
 	private KartaZerrenda sartuakJ1;
 	private KartaZerrenda sartuakCPU;
 	private static Tableroa nireTableroa;
+	private Connection konexioa;
 	
-	private Tableroa(){
+	private Tableroa() {
 		J1=new JokalariErreala();
 		CPU=new CPUJokalaria();
 		kola=new KartaZerrenda();
 		sartuakJ1=new KartaZerrenda();
 		sartuakCPU=new KartaZerrenda();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String zerbitzaria = "jdbc:mysql://localhost:3306/proiektua";
+		 String erabiltzailea = "root";
+		 String pasahitza = "";
+		 try {
+			this.konexioa =(Connection) DriverManager.getConnection(zerbitzaria, erabiltzailea, pasahitza);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		
 	}
 	
 	public JokalariErreala get(){
 		return this.J1;
 	}
-	
 	public KartaZerrenda getJ1(){
 		return this.J1.getBaraja();
 	}
-	
 	public KartaZerrenda getEskuanJ1(){
 		return this.J1.getEskuan();
 	}
-	
 	public KartaZerrenda getCPU(){
 		return this.CPU.getBaraja();
 	}
-	
 	public KartaZerrenda getEskuanCPU(){
 		return this.CPU.getEskuan();
 	}
-	
 	public KartaZerrenda getSartuakJ1(){
 		return this.sartuakJ1;
 	}
-	
 	public KartaZerrenda getSartuakCPU(){
 		return this.sartuakCPU;
 	}
-	
 	public KartaZerrenda getKola(){
 		return this.kola;
 	}
-	
-	public static Tableroa getNireTableroa(){
+	public static Tableroa getNireTableroa() {
 		if(nireTableroa==null){
 			nireTableroa=new Tableroa();
 		}
@@ -73,7 +88,6 @@ public class Tableroa extends Observable{
 	    if(this.kola.luzera()==5){
 	    	this.kolaEguneratu();
 	    }
-	    
 		CPU.kartaMahairaBota();
 		CPU.kartaBarajatikHartu();
 		animaladaRekurrenteak();
@@ -82,11 +96,11 @@ public class Tableroa extends Observable{
 	    }
 		ArrayList<String> bz=new ArrayList<String>();
 		for(int i=0;i<this.kola.luzera();i++){
-			bz.add(this.kola.getKarta(i).getKartaIrudia());
+			bz.add(this.kola.getKarta(i).getKarta());
 		}
 		bz.add("/");
 		for(int ind=0;ind<this.J1.getEskuan().luzera();ind++){
-			bz.add(this.J1.getEskuan().getKarta(ind).getKartaIrudia());
+			bz.add(this.J1.getEskuan().getKarta(ind).getKarta());
 		}
 		setChanged();
 		notifyObservers(bz);
@@ -135,7 +149,7 @@ public class Tableroa extends Observable{
 			}
 			if(aux!=null){
 				ind=kZ.posizioaLortu(aux);	
-			}else{
+			} else {
 				ind++;
 			}
 		}
@@ -153,6 +167,191 @@ public class Tableroa extends Observable{
 		this.sartuBereLekuan(k2);
 	}
 
+	public void erabiltzaileBerria(String erabilId, String pIzena, String pAbizena, String pEmail, String pUrtea, String pHilabetea, String pEguna, String pPasahitza) throws SQLException {
+		Statement st = konexioa.createStatement();
+		st.executeUpdate("INSERT INTO JOKALARIA VALUES ('"+erabilId+"','"+pIzena+"','"+pAbizena+"','ERABILTZAILEA','"+pPasahitza+"','"+pEmail+"',CURRENT_DATE,'"+pUrtea+"-"+pHilabetea+"-"+pEguna+"');");
+	}
+
+	public ArrayList<String> ranking(String pIzena) throws SQLException{
+		PreparedStatement st1=konexioa.prepareStatement("SELECT JOKALARIA_ID, PUNT_JOKALARIA, PUNT_AURKARIA, DATA, HORDUA, B_ORDUA FROM PARTIDA WHERE IRABAZI=1 AND JOKALARIA_ID='"+pIzena+"' ORDER BY PUNT_JOKALARIA DESC;");
+		ResultSet rs=st1.executeQuery();
+	
+		ResultSetMetaData rsmd=rs.getMetaData();
+		ArrayList<String> z=new ArrayList<String>();
+		int columnsNumber = rsmd.getColumnCount();
+	   	while (rs.next()) {
+	       		for (int o = 0; o <= columnsNumber; o++) {
+	           		if (o > 0){
+	           		String columnValue = rs.getString(o);
+	           		z.add(columnValue);
+				}
+	       		}
+		}
+	   	return z;
+	}
+
+	public ArrayList<String> ranking2(String pIzena) throws SQLException{
+		PreparedStatement st1=konexioa.prepareStatement("SELECT JOKALARIA_ID, PUNT_JOKALARIA, PUNT_AURKARIA, DATA, HORDUA, B_ORDUA FROM PARTIDA WHERE IRABAZI=1 AND DATA=CURRENT_DATE ORDER BY PUNT_JOKALARIA DESC;");
+		ResultSet rs=st1.executeQuery();
+	
+		ResultSetMetaData rsmd=rs.getMetaData();
+		ArrayList<String> z=new ArrayList<String>();
+		int columnsNumber = rsmd.getColumnCount();
+	   	while (rs.next()) {
+	       		for (int o = 0; o <= columnsNumber; o++) {
+	           		if (o > 0){
+	           			String columnValue = rs.getString(o);
+	           			z.add(columnValue);
+				}
+	       		}
+		}
+	   	return z;
+	}
+
+	public ArrayList<String> ranking3(String pIzena) throws SQLException{
+		PreparedStatement st1=konexioa.prepareStatement("SELECT JOKALARIA_ID, PUNT_JOKALARIA, PUNT_AURKARIA, DATA, HORDUA, B_ORDUA FROM PARTIDA WHERE IRABAZI=1 ORDER BY PUNT_JOKALARIA DESC;");
+		ResultSet rs=st1.executeQuery();
+	
+		ResultSetMetaData rsmd=rs.getMetaData();
+		ArrayList<String> z=new ArrayList<String>();
+		int columnsNumber = rsmd.getColumnCount();
+	   	while (rs.next()) {
+	       		for (int o = 0; o <= columnsNumber; o++) {
+	           		if (o > 0){
+	           			String columnValue = rs.getString(o);
+	           			z.add(columnValue);
+				}
+	       		}
+		}
+	   	return z;
+	}
+
+	public ArrayList<String> ranking4(String pIzena) throws SQLException{
+		PreparedStatement st1=konexioa.prepareStatement("SELECT JOKALARIA_ID, AVG(PUNT_JOKALARIA) FROM PARTIDA WHERE IRABAZI=1  GROUP BY JOKALARIA_ID ORDER BY AVG(PUNT_JOKALARIA) DESC;");
+		ResultSet rs=st1.executeQuery();
+	
+		ResultSetMetaData rsmd=rs.getMetaData();
+		ArrayList<String> z=new ArrayList<String>();
+		int columnsNumber = rsmd.getColumnCount();
+	  	while (rs.next()) {
+	       		for (int o = 0; o <= columnsNumber; o++) {
+	           		if (o > 0){
+	          	 		String columnValue = rs.getString(o);
+	           			z.add(columnValue);
+				}
+	       		}
+		}
+	   return z;
+	}
+
+	public boolean ondoDago(String erabiltzailea,String pasahitza) throws SQLException{
+		PreparedStatement st1=konexioa.prepareStatement("SELECT ERABILTZAILE_IZENA FROM JOKALARIA WHERE ERABILTZAILE_IZENA='"+erabiltzailea+"' AND PASAHITZA='"+pasahitza+"';");
+		ResultSet rs=st1.executeQuery();
+	
+		ResultSetMetaData rsmd=rs.getMetaData();
+		ArrayList<String> z=new ArrayList<String>();
+		int columnsNumber = rsmd.getColumnCount();
+	   	while (rs.next()) {
+	       		for (int o = 0; o <= columnsNumber; o++) {
+	          		if (o > 0){
+	           			String columnValue = rs.getString(o);
+	          			 z.add(columnValue);
+				}
+	       		}
+		}
+	 	if(z.size()==0){
+			return false;
+		} else {
+		 	return true;
+	 	}
+	}
+
+	public boolean existitzenDa(String erabiltzailea) throws SQLException{
+		PreparedStatement st1=konexioa.prepareStatement("SELECT ERABILTZAILE_IZENA FROM JOKALARIA WHERE ERABILTZAILE_IZENA='"+erabiltzailea+"';");
+		ResultSet rs=st1.executeQuery();
+	
+		ResultSetMetaData rsmd=rs.getMetaData();
+		ArrayList<String> z=new ArrayList<String>();
+		int columnsNumber = rsmd.getColumnCount();
+	   	while (rs.next()) {
+	       		for (int o = 0; o <= columnsNumber; o++) {
+	           		if (o > 0){
+	           			String columnValue = rs.getString(o);
+	           			z.add(columnValue);
+				}
+	       		}
+		}
+		if(z.size()==0){
+			return false;
+	 	} else {
+		 	return true;
+		}
+	}
+
+	public void administratzaileaAldatu(String erabiltzailea, String pMota) throws SQLException{
+		Statement st1=konexioa.createStatement();
+		st1.executeUpdate("UPDATE JOKALARIA SET ERABILTZAILE_MOTA='"+pMota+"' WHERE ERABILTZAILE_IZENA = '"+erabiltzailea+"';");
+	}
+
+	public boolean administratzaileaDa(String erabiltzailea) throws SQLException{
+		PreparedStatement st1=konexioa.prepareStatement("SELECT ERABILTZAILE_IZENA FROM JOKALARIA WHERE ERABILTZAILE_IZENA='"+erabiltzailea+"' AND ERABILTZAILE_MOTA='ADMINISTRATZAILEA';");
+		ResultSet rs=st1.executeQuery();
+	
+		ResultSetMetaData rsmd=rs.getMetaData();
+		ArrayList<String> z=new ArrayList<String>();
+		int columnsNumber = rsmd.getColumnCount();
+	 	while (rs.next()) {
+	      	for (int o = 0; o <= columnsNumber; o++) {
+	        	if (o > 0){
+	          		String columnValue = rs.getString(o);
+	           		z.add(columnValue);}
+	       		}
+		}
+	 	if(z.size()==0){
+			return false;
+		} else {
+			return true;
+		 }
+	}
+
+	public void baneatu(String izena) throws SQLException{
+		Statement st1=konexioa.createStatement();
+		st1.executeUpdate("DELETE FROM PARTIDA WHERE JOKALARIA_ID = '"+izena+"';");
+		st1.executeUpdate("DELETE FROM JOKALARIA WHERE ERABILTZAILE_IZENA = '"+izena+"';");	
+	}
+
+	public void partidaGorde(String hOrdua, int puntuazioaJ, int puntuazioaCPU, String erabiltzailea) throws SQLException{
+		Statement st1=konexioa.createStatement();
+		int irabazi;
+		if(puntuazioaJ>puntuazioaCPU){
+			irabazi=1;
+		} else {
+			irabazi=0;
+		}
+		String s=new String();
+		boolean aurkitua=false;
+		for (int n = 0; n <hOrdua.length (); n++) { 
+			char c = hOrdua.charAt (n);
+			if(c=='.'){
+				aurkitua=false;
+			}
+			if(aurkitua){
+				s=s+c;
+			}
+			if(c=='T'){
+				aurkitua=true;
+			}
+		}
+		int a=(int) (Math.random()*1000000000)+1;
+		st1.executeUpdate("INSERT INTO PARTIDA VALUES("+a+",'URDINA',CURRENT_DATE,'"+s+"',CURRENT_TIME,"+puntuazioaJ+","+puntuazioaCPU+",'"+erabiltzailea+"',"+irabazi+");");
+	}
+
+	public void kartaSortu(String izena, String zenbakia, String azalpena, String puntuazioa, String originala) throws SQLException{
+		Statement st1=konexioa.createStatement();
+		st1.executeUpdate("INSERT INTO KARTA VALUES('"+izena+"',"+zenbakia+",'"+azalpena+"',"+puntuazioa+","+originala+");");
+	}
+
+
 	public String irabazleaLortu(){
 		int punt1=0;
 		int punt2=0;
@@ -163,17 +362,32 @@ public class Tableroa extends Observable{
 			punt2=punt2+this.sartuakCPU.getKarta(j).getBalioa();
 		}
 		if (punt1>punt2){
-			return "J1 irabazi du";
-		}
-		else{
+			return "J1 irabazi du   J1:"+punt1+"   CPU:"+punt2;
+		} else {
 			if(punt1<punt2){
-				return "CPU irabazi du";
+				return "CPU irabazi du   J1:"+punt1+"   CPU:"+punt2;
+			} else {
+				return "Berdinketa egon da   J1:"+punt1+"   CPU:"+punt2;
 			}
-			else{
-				return "Berdinketa egon da";
-			}
-		}	
+		}
 	}
+
+	public int puntuazioaJ(){
+		int punt1=0;
+		for(int i=0;i<this.sartuakJ1.luzera();i++){
+			punt1=punt1+this.sartuakJ1.getKarta(i).getBalioa();
+		}
+		return punt1;
+	}
+
+	public int puntuazioaCPU(){
+		int punt1=0;
+		for(int j=0;j<this.sartuakCPU.luzera();j++){
+			punt1=punt1+this.sartuakCPU.getKarta(j).getBalioa();
+		}
+		return punt1;
+	}
+
 
 	public void sartuBereLekuan(Karta k1){
 		if(k1.getKolorea().equals("Urdina")){
@@ -197,13 +411,13 @@ public class Tableroa extends Observable{
 		CPU.jokalariaHasieratu("Berdea");
 		ArrayList<String> bz=new ArrayList<String>();
 		for(int i=0;i<this.kola.luzera();i++){
-			bz.add(this.kola.getKarta(i).getKartaIrudia());
+			bz.add(this.kola.getKarta(i).getKarta());
 		}
 		bz.add("/");
 		for(int ind=0;ind<this.J1.getEskuan().luzera();ind++){
-			bz.add(this.J1.getEskuan().getKarta(ind).getKartaIrudia());
+			bz.add(this.J1.getEskuan().getKarta(ind).getKarta());
 		}
 		setChanged();
-		notifyObservers(bz);		
-	}
+		notifyObservers(bz);	
+	}	
 }
