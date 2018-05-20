b4da453a18f66cc20b535f94f2b2f5379646b629
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-20 21:09:46 +0200

    Update KanguroKarta.java

--- src/packProiektua/KanguroKarta.java
+++ src/packProiektua/KanguroKarta.java
@@ -1,9 +1,9 @@
 package packProiektua;
 
 public class KanguroKarta extends Karta{
-	
+		
 	public KanguroKarta(String pKolorea){
-		super("Kanguroa",3,false,pKolorea,"<html>Jokoan sartzean,<br> aurretik dituen animalia<br> bat edo biren gainetik<br> pasatuko da,<br> jokalariaren araberakoa.<br><br><br><br></html>",4);
+		super("Kanguroa",3,false,pKolorea,"<html>Jokoan sartzean,<br> aurretik dituen animalia<br> bat edo biren gainetik<br> pasatuko da,<br> jokalariaren araberakoa.<br><br><br><br></html>",4,"C:/Kartak/Kanguro"+pKolorea+".png");
 		this.ia= new KanguroAnimalada();
 	}
 	
