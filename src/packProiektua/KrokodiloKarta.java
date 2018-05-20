da0266ca567e2fd846cd3897a1924ab0d98f8099
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-20 21:13:58 +0200

    Update KrokodiloKarta.java

--- src/packProiektua/KrokodiloKarta.java
+++ src/packProiektua/KrokodiloKarta.java
@@ -3,7 +3,7 @@ package packProiektua;
 public class KrokodiloKarta extends Karta{
 	
 	public KrokodiloKarta(String pKolorea){
-		super("Krokodiloa",10,true,pKolorea, "<html>Jokoan sartzean,<br> bera baino balio<br> txikiagoak dituen kartak<br> jan, eta kalera<br> botatzen ditu.<br> Beti ere Zebra<br> bat aurretik ez badauka.<br> Errepikakorra da.</html>",3);
+		super("Krokodiloa",10,true,pKolorea, "<html>Jokoan sartzean,<br> bera baino balio<br> txikiagoak dituen kartak<br> jan, eta kalera<br> botatzen ditu.<br> Beti ere Zebra<br> bat aurretik ez badauka.<br> Errepikakorra da.</html>",3,"C:/Kartak/Krokodilo"+pKolorea+".png");
 		this.ia= new KrokodiloAnimalada();
 	}
 	
