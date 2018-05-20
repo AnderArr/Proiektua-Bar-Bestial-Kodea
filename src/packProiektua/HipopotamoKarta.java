5b70ca755047fc34d58f64fc63bd04b8c63181d5
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-20 21:04:33 +0200

    Update HipopotamoKarta.java

--- src/packProiektua/HipopotamoKarta.java
+++ src/packProiektua/HipopotamoKarta.java
@@ -3,7 +3,7 @@ package packProiektua;
 public class HipopotamoKarta extends Karta{
 	
 	public HipopotamoKarta(String pKolorea){
-		super("Hipopotamoa",11,true,pKolorea,"<html>Jokoan sartzean,<br> bere balioa baino<br> txikiagoak diren<br> karten aurretik<br> kokatzen da,<br> beti ere Zebra<br> bat aurretik ez badago.<br> Errepikakorra da.</html>",2);
+		super("Hipopotamoa",11,true,pKolorea,"<html>Jokoan sartzean,<br> bere balioa baino<br> txikiagoak diren<br> karten aurretik<br> kokatzen da,<br> beti ere Zebra<br> bat aurretik ez badago.<br> Errepikakorra da.</html>",2,"C:/Kartak/Hipopotamo"+pKolorea+".png");
 		this.ia= new HipopotamoAnimalada();
 		}
 		
