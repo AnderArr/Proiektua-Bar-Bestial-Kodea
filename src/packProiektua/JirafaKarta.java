65958922f81610834c569f87aa2009fe48c9031f
AnderArr <37108749+AnderArr@users.noreply.github.com>
2018-05-20 21:05:43 +0200

    Update JirafaKarta.java

--- src/packProiektua/JirafaKarta.java
+++ src/packProiektua/JirafaKarta.java
@@ -2,8 +2,9 @@ package packProiektua;
 
 public class JirafaKarta extends Karta{
 
+	
 	public JirafaKarta(String pKolorea){
-		super("Jirafa",8,true,pKolorea,"<html>Jokoan sartzean,<br> aurrean duen kartaren<br> gainetik pasako da,<br> bere balioa Jirafarena<br> baino txikiagoa bada.<br> Errepikakorra da.<br><br><br></html>",3);
+		super("Jirafa",8,true,pKolorea,"<html>Jokoan sartzean,<br> aurrean duen kartaren<br> gainetik pasako da,<br> bere balioa Jirafarena<br> baino txikiagoa bada.<br> Errepikakorra da.<br><br><br></html>",3,"C:/Kartak/Jirafa"+pKolorea+".png");
 		this.ia=new JirafaAnimalada();
 	}
 	
